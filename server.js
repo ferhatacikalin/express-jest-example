4d0c845b38c4400b6d11efaeebf03fd0f542fab9
Ferhat Açıkalın <ferhatackln@gmail.com>
2024-03-21 01:46:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const app = require('./src/app.js');
 
 const port = 3000;
 
-app.listen(port, () => { console.log(`Server has started on port ${port}`); }); // eslint-disable-line no-console
+app.listen(port, () => { console.log(`Server has started on port ${port}
+
+`); }); // eslint-disable-line no-console
